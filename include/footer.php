cb69ed24f69bace7d0a7bb1a1930c0b8f03969dd
Maxsuel Fernando <maxsuel.dev@gmail.com>
2021-04-22 22:14:51 -0300

    update repo

---
+++ include/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+	<div class="container12">
+		<ul class="redes_sociais">
+			<a href=""><li class="facebook_logo"><i class="fa fa-facebook"></i></li></a>
+
+			<a href=""><li class="instagram_logo"><i class="fa fa-instagram"></i></li></a>
+		</ul>
+	</div>
+	<div class="clear"></div>
+	<div class="container12">
+		<div class="number column4">	
+			<p>(82) XXXXX-XXXX</p>
+			<p>(82) XXXXX-XXXX</p>
+		</div>
+		<p class="column4">Rua xxxxxxx xxxx xxxxx, xxxxxx xxxxx xxx, xxxxxxxxxx.</p>
+		<p class="column4">contato@escolacreative.com</p>
+	</div>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,10 @@
+	<div class="container16">
+		<div class="icon"><div class="hamburger"></div></div>
+			
+		<div class="column2">
+			<div class="header_logo">
+				<a href="">
+					<img class="img_logo" src="images/logo.png" alt="Creative_logo" width="100px;" height="100px">
+				</a>
+			</div>
+		</div>
